be1d0a9ee6920bb7234841c018a744ce086efb01
Simon Katan <s.katan@gold.ac.uk>
2022-04-27 13:22:52 +0100

    added index page with form

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const assert = require('assert');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 const cardTypes = ["birthday", "leaving"];
 
-app.use(express.static('/public'));
+app.use(express.static('public'));
 
 app.get('/', (req,res)=>{
-    res.send('Welcome to Greetings Card Maker !');
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
-//to call use http://localhost:3000/greetings-card?cardtype=birthday&name=simon
+//to call use http://localhost:3000/greetings-card?cardtype=birthday&recipient=simon
 
 app.get('/greetings-card', (req,res)=>{
 
     assert(cardTypes.includes(req.query.cardtype),"invalid card type");
-    assert(req.query.name,"you need to provide a name");
+    assert(req.query.recipient,"you need to provide a recipient");
 
     if(req.query.cardtype === "birthday"){
-        res.send(`Happy Birthday ${req.query.name}!`);
+        res.send(`Happy Birthday ${req.query.recipient}!`);
     }else if(req.query.cardtype === "leaving"){
-        res.send(`Sorry you're leaving ${req.query.name}! `);
+        res.send(`Sorry you're leaving ${req.query.recipient}! `);
     }
 })
 
