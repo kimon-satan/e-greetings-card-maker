7dd0023a34fc91442bb8b95da177933b2812ecd8
Simon Katan <s.katan@gold.ac.uk>
2022-04-27 15:57:59 +0100

    redirect and store parameters

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 const cardTypes = ["birthday", "leaving"];
-const userCards = [];
+const userCards = new Map();
 
 ////////////////////////////////////// HELPER FUNCTIONS //////////////////////////////////////
 
@@ -24,6 +24,9 @@ const generateId = (numChars=12)=>{
 
 app.set('view engine', 'ejs');
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.use(express.static('public'));
 
 
@@ -33,25 +36,34 @@ app.get('/', (req,res)=>{
 
 //to call use http://localhost:3000/greetings-card?cardtype=birthday&recipient=simon
 
-app.get('/greetings-card', (req,res)=>{
+app.get('/edit-greetings-card', (req,res)=>{
 
-    assert(cardTypes.includes(req.query.cardtype),"invalid card type");
-    assert(req.query.recipient,"you need to provide a recipient");
-
-    if(req.query.cardid){
-        assert(userCards.includes(req.query.cardid),"cardid not found");
-        if(req.query.cardtype === "birthday"){
-            //res.send(`Happy Birthday ${req.query.recipient}!`);
-            res.render('birthday-card',{recipient: capitalize(req.query.recipient), cardId: req.query.cardid});
-        }else if(req.query.cardtype === "leaving"){
-            res.render('leaving-card',{recipient: capitalize(req.query.recipient), cardId: req.query.cardid});
-            //res.send(`Sorry you're leaving ${req.query.recipient}! `);
-        }
-    }else{
-        const cardId = generateId();
-        userCards.push(cardId);
-        res.redirect(`/greetings-card?cardtype=${req.query.cardtype}&recipient=${req.query.recipient}&cardid=${cardId}`);
+    assert(req.query.cardid,"no cardid provided");
+    const cardDetails = userCards.get(req.query.cardid);
+    assert(cardDetails,"cardid not found");
+    
+    if(cardDetails.cardType === "birthday"){
+        //res.send(`Happy Birthday ${req.query.recipient}!`);
+        res.render('birthday-card',{recipient: capitalize(cardDetails.recipient), cardId: cardDetails.cardId});
+    }else if(cardDetails.cardType === "leaving"){
+        res.render('leaving-card',{recipient: capitalize(cardDetails.recipient), cardId: cardDetails.cardid});
+        //res.send(`Sorry you're leaving ${req.query.recipient}! `);
     }
+    
+})
+
+app.post('/create-greetings-card', (req,res)=>{
+
+    assert(cardTypes.includes(req.body.cardtype),"invalid card type");
+    assert(req.body.recipient,"you need to provide a recipient");
+
+    const cardId = generateId();
+    const cardDetails = {recipient: req.body.recipient, cardType: req.body.cardtype};
+    userCards.set(cardId, cardDetails);
+
+    //forward to the edit page
+    res.redirect(`/edit-greetings-card?cardid=${cardId}`);
+
 })
 
 app.listen(port, ()=>console.log(`listening on port ${port}`));
