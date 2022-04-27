f365150f9677512b029fbf4190ef95c748a2b21a
Simon Katan <s.katan@gold.ac.uk>
2022-04-27 14:53:31 +0100

    create and store cardid

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ const app = express();
 const port = 3000;
 
 const cardTypes = ["birthday", "leaving"];
+const userCards = [];
 
 ////////////////////////////////////// HELPER FUNCTIONS //////////////////////////////////////
 
 const capitalize = s=>s.charAt(0).toUpperCase() + s.slice(1);
+
+const generateId = (numChars=12)=>{
+    const chars = "./ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$";
+    let s = "";
+    for(let i = 0; i < numChars; i++){
+        s += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return s;
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -28,12 +38,19 @@ app.get('/greetings-card', (req,res)=>{
     assert(cardTypes.includes(req.query.cardtype),"invalid card type");
     assert(req.query.recipient,"you need to provide a recipient");
 
-    if(req.query.cardtype === "birthday"){
-        //res.send(`Happy Birthday ${req.query.recipient}!`);
-        res.render('birthday-card',{recipient: capitalize(req.query.recipient)});
-    }else if(req.query.cardtype === "leaving"){
-        res.render('leaving-card',{recipient: capitalize(req.query.recipient)});
-        //res.send(`Sorry you're leaving ${req.query.recipient}! `);
+    if(req.query.cardid){
+        assert(userCards.includes(req.query.cardid),"cardid not found");
+        if(req.query.cardtype === "birthday"){
+            //res.send(`Happy Birthday ${req.query.recipient}!`);
+            res.render('birthday-card',{recipient: capitalize(req.query.recipient), cardId: req.query.cardid});
+        }else if(req.query.cardtype === "leaving"){
+            res.render('leaving-card',{recipient: capitalize(req.query.recipient), cardId: req.query.cardid});
+            //res.send(`Sorry you're leaving ${req.query.recipient}! `);
+        }
+    }else{
+        const cardId = generateId();
+        userCards.push(cardId);
+        res.redirect(`/greetings-card?cardtype=${req.query.cardtype}&recipient=${req.query.recipient}&cardid=${cardId}`);
     }
 })
 
