50df0477f386c060996cb67c3cf0b685c080699f
Simon Katan <simon@simonkatan.co.uk>
2022-11-16 09:13:05 +0000

    emptied template for webinar1

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const cardTypes = ["birthday", "leaving"];
-const userCards = new Map();
+
 
 global.url = `http://localhost:${port}`
 
@@ -22,96 +21,35 @@ const generateId = (numChars=12)=>{
     return s;
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-app.set('view engine', 'ejs');
-
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-app.use(express.static('public'));
+//////////////////////////////////////// DATA STRUCTURES ////////////////////////////////
 
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.send("Hello world");
 })
 
 
 app.get('/edit-greetings-card', (req,res)=>{
 
-    assert(req.query.cardId,"no cardId provided");
-    const cardDetails = userCards.get(req.query.cardId);
-    assert(cardDetails,"cardId not found");
+    res.send("edit-greetings-card");
 
-    const params = {
-        recipient: capitalize(cardDetails.recipient), 
-        cardId: req.query.cardId, 
-        url: global.url,
-        messages: cardDetails.messages,
-        isEdit: true
-    };
-    
-    if(cardDetails.cardType === "birthday"){
-        res.render('birthday-card', params);
-    }else if(cardDetails.cardType === "leaving"){
-        res.render('leaving-card', params);
-    }  
 })
 
 app.get('/view-greetings-card', (req,res)=>{
 
-    assert(req.query.cardId,"no cardId provided");
-    const cardDetails = userCards.get(req.query.cardId);
-    assert(cardDetails,"cardId not found");
+    res.send("view-greetings-card");
 
-
-    const params = {
-        recipient: capitalize(cardDetails.recipient), 
-        cardId: req.query.cardId, 
-        url: global.url,
-        messages: cardDetails.messages,
-        isEdit: false
-    };
-    
-    if(cardDetails.cardType === "birthday"){
-        res.render('birthday-card',params);
-    }else if(cardDetails.cardType === "leaving"){
-        res.render('leaving-card',params);
-    }  
 })
 
 
 app.post('/create-greetings-card', (req,res)=>{
 
-    assert(cardTypes.includes(req.body.cardtype),"invalid card type");
-    assert(req.body.recipient,"you need to provide a recipient");
-
-    const cardId = generateId();
-    const cardDetails = {
-        recipient: req.body.recipient, 
-        cardType: req.body.cardtype,
-        messages: []
-    };
-    userCards.set(cardId, cardDetails);
-
-    //forward to the edit page
-    res.redirect(`/edit-greetings-card?cardId=${cardId}`);
-
+    res.send("POST create-greetings-card");
 })
 
 app.post('/add-message', (req,res)=>{
 
-    assert(req.body.cardId,"no cardId provided");
-    const cardDetails = userCards.get(req.body.cardId);
-    assert(cardDetails,"cardId not found");
-
-    const t = {...cardDetails};
-
-    t.messages.push({message: req.body.message, from: req.body.from});
-
-    userCards.set(req.body.cardId, t);
-
-    res.redirect(`/edit-greetings-card?cardId=${req.body.cardId}`);
+    res.send("POST add-message");
 })
 
 
