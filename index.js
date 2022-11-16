82ac9b88a324d662f1e25e18398c7240a0633244
Simon Katan <simon@simonkatan.co.uk>
2022-11-16 09:27:50 +0000

    removed ejs dependency

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-
-
-global.url = `http://localhost:${port}`
-
 ////////////////////////////////////// HELPER FUNCTIONS //////////////////////////////////////
 
 const capitalize = s=>s.charAt(0).toUpperCase() + s.slice(1);
@@ -24,34 +20,45 @@ const generateId = (numChars=12)=>{
 //////////////////////////////////////// DATA STRUCTURES ////////////////////////////////
 
 
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @description This renders the front page of the website
+ */
 app.get('/', (req,res)=>{
     res.send("Hello world");
 })
 
-
+/**
+ * @description This renders the edit page of the website
+ * @param cardId
+ */
 app.get('/edit-greetings-card', (req,res)=>{
-
     res.send("edit-greetings-card");
-
 })
 
+/**
+ * @description This renders the viewing page of the website
+ * @param cardId
+ */
 app.get('/view-greetings-card', (req,res)=>{
-
     res.send("view-greetings-card");
-
 })
 
-
+/**
+ * @description This creates a new creating card record then redirects to it's edit page
+ */
 app.post('/create-greetings-card', (req,res)=>{
-
     res.send("POST create-greetings-card");
 })
 
+/**
+ * @description This appends a message to a greeting card
+ */
 app.post('/add-message', (req,res)=>{
-
     res.send("POST add-message");
 })
 
 
 app.listen(port, ()=>console.log(`listening on port ${port}`));
-
